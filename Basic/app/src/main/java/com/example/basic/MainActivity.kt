27729cedef518e4da3850f9251d13d27c408ae8c
kyunghwa <snoopn303@gmail.com>
2023-10-09 18:08:08 +0900

    remember

--- Basic/app/src/main/java/com/example/basic/MainActivity.kt
+++ Basic/app/src/main/java/com/example/basic/MainActivity.kt
@@ -1,71 +1,59 @@
 package com.example.basic
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.basic.ui.theme.BasicTheme
+
+
+// remember
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BasicTheme {
-                MyBtn()
+                SimpleCounterBtn()
             }
         }
     }
 }
 
 @Composable
-fun MyBtn() {
-    val context = LocalContext.current
-    Button(
-        onClick = {
-            Log.d("Main", "onClick")
-            Toast.makeText(context, "클릭", Toast.LENGTH_LONG).show()
-        },
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Black,
-            contentColor = Color.Red
-        ),
-        modifier = Modifier
-            .width(200.dp)
-            .height(300.dp)
+fun SimpleCounterBtn() {
+    var count by remember {
+        mutableStateOf(0)
+    }
+
+    Button(onClick = {
+        count++
+    },
+        modifier = Modifier.fillMaxSize()
     ) {
         Text(
-            text = "ButtonButtonButtonButtonButtonButton",
-            lineHeight = 30.sp,
-            fontSize = 30.sp,
-            color = Color.White
+            text = "Count :$count",
+            fontSize = 50.sp
         )
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BasicTheme {
-        MyBtn()
+        SimpleCounterBtn()
     }
 }
