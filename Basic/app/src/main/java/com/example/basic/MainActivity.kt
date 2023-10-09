3dcf314581ef0f039f43da71d3c3e76b5f19bb97
kyunghwa <snoopn303@gmail.com>
2023-10-09 17:37:54 +0900

    Text

--- Basic/app/src/main/java/com/example/basic/MainActivity.kt
+++ Basic/app/src/main/java/com/example/basic/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.basic.ui.theme.BasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,11 +29,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyTextEx()
                 }
             }
         }
     }
+}
+
+@Composable
+fun MyTextEx() {
+
+    // text
+    // fontSize
+    // color
+    // fontStyle
+    // fontWeight
+    // modifier(padding)
+    // background
+
+    Text(
+        text = "Text 입니다",
+        fontSize = 30.sp,
+        color = Color.Red,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(30.dp),
+        style = TextStyle(background = Color.Blue)
+    )
 }
 
 @Composable
@@ -41,6 +70,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BasicTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            MyTextEx()
+        }
     }
 }
