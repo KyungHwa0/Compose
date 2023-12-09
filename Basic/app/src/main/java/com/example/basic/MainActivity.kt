f885b8415d8ae4c6f73dcf98bc704ba67796aed4
kyunghwa <snoopn303@gmail.com>
2023-12-09 21:05:50 +0900

    url 불러오기

--- Basic/app/src/main/java/com/example/basic/MainActivity.kt
+++ Basic/app/src/main/java/com/example/basic/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.basic.ui.theme.BasicTheme
 
 
@@ -33,7 +34,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicTheme {
-                MyImage()
+                MyImageWeb()
             }
         }
     }
@@ -47,10 +48,21 @@ fun MyImage() {
     )
 }
 
+
+//https://private-user-images.githubusercontent.com/124041716/284048454-23926794-adb1-4632-b800-f31a7d4d45c3.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDIxMjM1MTMsIm5iZiI6MTcwMjEyMzIxMywicGF0aCI6Ii8xMjQwNDE3MTYvMjg0MDQ4NDU0LTIzOTI2Nzk0LWFkYjEtNDYzMi1iODAwLWYzMWE3ZDRkNDVjMy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMxMjA5JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMTIwOVQxMjAwMTNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02MDMxMmQ3YmI0ZWYzMTBhOThkYzhlMDJiOTNlMmY5M2UzYmFhMmIwNzE3MzA1ZDhlN2NiMzEwNjUyYmI5Yjk4JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.VlqWjq-gyvmjXqNMM3uaeQyat_ZxBvyNoiaE8pobXvY
+@Composable
+fun MyImageWeb() {
+    AsyncImage(
+        model = "https://private-user-images.githubusercontent.com/124041716/284048454-23926794-adb1-4632-b800-f31a7d4d45c3.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDIxMjM1MTMsIm5iZiI6MTcwMjEyMzIxMywicGF0aCI6Ii8xMjQwNDE3MTYvMjg0MDQ4NDU0LTIzOTI2Nzk0LWFkYjEtNDYzMi1iODAwLWYzMWE3ZDRkNDVjMy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMxMjA5JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMTIwOVQxMjAwMTNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02MDMxMmQ3YmI0ZWYzMTBhOThkYzhlMDJiOTNlMmY5M2UzYmFhMmIwNzE3MzA1ZDhlN2NiMzEwNjUyYmI5Yjk4JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.VlqWjq-gyvmjXqNMM3uaeQyat_ZxBvyNoiaE8pobXvY",
+        contentDescription = "두툼",
+        modifier = Modifier.fillMaxSize()
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BasicTheme {
-        MyImage()
+        MyImageWeb()
     }
 }
