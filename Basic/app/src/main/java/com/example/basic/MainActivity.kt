dd1f16a2f359615de56f6dfa535980b15bcd602f
kyunghwa <snoopn303@gmail.com>
2023-12-12 19:34:24 +0900

    박스 배치 실습

--- Basic/app/src/main/java/com/example/basic/MainActivity.kt
+++ Basic/app/src/main/java/com/example/basic/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
@@ -26,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.basic.ui.theme.BasicTheme
@@ -49,7 +52,106 @@ fun BoxExample() {
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color.Cyan)
-    )
+    ) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+                .padding(12.dp)
+                .align(Alignment.TopStart)
+        ) {
+            Text(
+                text = "왼쪽 위",
+                color = Color.White
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+                .padding(12.dp)
+                .align(Alignment.TopCenter)
+        ) {
+            Text(
+                text = "중앙 위",
+                color = Color.White
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+                .padding(12.dp)
+                .align(Alignment.TopEnd)
+        ) {
+            Text(
+                text = "오른쪽 위",
+                color = Color.White
+            )
+        }
+
+        Button(
+            onClick = { },
+            modifier = Modifier.align(Alignment.CenterStart)
+        ) {
+            Text(text = "중앙 왼쪽")
+        }
+
+        Button(
+            onClick = { },
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text(text = "중앙")
+        }
+
+        Button(
+            onClick = { },
+            modifier = Modifier.align(Alignment.CenterEnd)
+        ) {
+            Text(text = "중앙 오른쪽")
+        }
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+                .padding(12.dp)
+                .align(Alignment.BottomStart)
+        ) {
+            Text(
+                text = "왼쪽 아래",
+                color = Color.White
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+                .padding(12.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            Text(
+                text = "중앙 아래",
+                color = Color.White
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+                .padding(12.dp)
+                .align(Alignment.BottomEnd)
+        ) {
+            Text(
+                text = "오른쪽 아래",
+                color = Color.White
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
