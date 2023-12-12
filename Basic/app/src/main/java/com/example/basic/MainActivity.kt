b9359bbf0d5d31064331efddb21057f11d63c94c
kyunghwa <snoopn303@gmail.com>
2023-12-12 19:22:37 +0900

    Box 실습

--- Basic/app/src/main/java/com/example/basic/MainActivity.kt
+++ Basic/app/src/main/java/com/example/basic/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,6 +23,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -28,34 +31,24 @@ import coil.compose.AsyncImage
 import com.example.basic.ui.theme.BasicTheme
 
 
-// Image
+// Box
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BasicTheme {
-                MyImageWeb()
+                BoxExample()
             }
         }
     }
 }
 
 @Composable
-fun MyImage() {
-    Image(
-        painter = painterResource(id = R.drawable.pro),
-        contentDescription = "pro"
-    )
-}
-
-
-//https://private-user-images.githubusercontent.com/124041716/284048454-23926794-adb1-4632-b800-f31a7d4d45c3.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDIxMjM1MTMsIm5iZiI6MTcwMjEyMzIxMywicGF0aCI6Ii8xMjQwNDE3MTYvMjg0MDQ4NDU0LTIzOTI2Nzk0LWFkYjEtNDYzMi1iODAwLWYzMWE3ZDRkNDVjMy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMxMjA5JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMTIwOVQxMjAwMTNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02MDMxMmQ3YmI0ZWYzMTBhOThkYzhlMDJiOTNlMmY5M2UzYmFhMmIwNzE3MzA1ZDhlN2NiMzEwNjUyYmI5Yjk4JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.VlqWjq-gyvmjXqNMM3uaeQyat_ZxBvyNoiaE8pobXvY
-@Composable
-fun MyImageWeb() {
-    AsyncImage(
-        model = "https://private-user-images.githubusercontent.com/124041716/284048454-23926794-adb1-4632-b800-f31a7d4d45c3.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDIxMjM1MTMsIm5iZiI6MTcwMjEyMzIxMywicGF0aCI6Ii8xMjQwNDE3MTYvMjg0MDQ4NDU0LTIzOTI2Nzk0LWFkYjEtNDYzMi1iODAwLWYzMWE3ZDRkNDVjMy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMxMjA5JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMTIwOVQxMjAwMTNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02MDMxMmQ3YmI0ZWYzMTBhOThkYzhlMDJiOTNlMmY5M2UzYmFhMmIwNzE3MzA1ZDhlN2NiMzEwNjUyYmI5Yjk4JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.VlqWjq-gyvmjXqNMM3uaeQyat_ZxBvyNoiaE8pobXvY",
-        contentDescription = "두툼",
-        modifier = Modifier.fillMaxSize()
+fun BoxExample() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Cyan)
     )
 }
 
@@ -63,6 +56,6 @@ fun MyImageWeb() {
 @Composable
 fun GreetingPreview() {
     BasicTheme {
-        MyImageWeb()
+        BoxExample()
     }
 }
