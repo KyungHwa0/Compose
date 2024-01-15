88004b1e5cd13a912703d91c68166224f31d944f
kyunghwa <snoopn303@gmail.com>
2024-01-16 01:34:33 +0900

    리팩토링

--- ProfileImage/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ ProfileImage/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,167 +29,164 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.platform.LocalView
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             MyApplicationTheme {
-                Surface(
-                    // 해당 컴포넌트에서는 머티리얼 테마를 사용
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ProfileImage()
-                }
+                ProfileImage()
             }
-        }
-    }
-
-    @Composable
-    fun ColumnRowTest1() {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(20.dp)
-                .background(Color.Gray)
-        ) {
-            Text(
-                text = "안녕ㅎ",
-                color = Color.White,
-                fontSize = 20.sp
-            )
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-//            horizontalArrangement = Arrangement.SpaceAround
-//            horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Text(text = "왼쪽")
-                Text(text = "중앙")
-                Text(text = "오른쪽")
-            }
-
-            Text(
-                text = "하이",
-                color = Color.Blue,
-                fontSize = 20.sp
-            )
-        }
-    }
-
-    @Composable
-    fun ProfileImage() {
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-//            .padding(5.dp)
-                .background(color = Color(0xFF000000))
-                .padding(10.dp)
-                .border(
-                    border = BorderStroke(5.dp, color = Color(0xFF323232))
-                ),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-
-            Box(
-                modifier = Modifier.padding(top = 50.dp)
-
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.mokoko1),
-                    contentDescription = "콰앙",
-                    modifier = Modifier
-                        .size(200.dp)
-                )
-            }
-
-            Text(
-                text = "Android Developer",
-                color = Color.White,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(20.dp)
-            )
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 20.dp)
-            ) {
-                Text(
-                    text = "Email",
-                    color = Color.White,
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(10.dp)
-                )
-
-                Text(
-                    text = "snoopn@naver.com",
-                    color = Color.White,
-                    fontSize = 14.sp,
-                    modifier = Modifier.padding(10.dp)
-                )
-            }
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 20.dp)
-
-            ) {
-                Text(
-                    text = "Github",
-                    color = Color.White,
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(10.dp)
-                )
-
-                Text(
-                    text = "https://github.com/KyungHwa0",
-                    color = Color.White,
-                    fontSize = 14.sp,
-                    modifier = Modifier.padding(10.dp)
-                )
-            }
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 20.dp)
-            ) {
-                Text(
-                    text = "Blog",
-                    color = Color.White,
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(10.dp)
-                )
-
-                Text(
-                    text = "https://wagzack.tistory.com/",
-                    color = Color.White,
-                    fontSize = 14.sp,
-                    modifier = Modifier.padding(10.dp)
-                )
-            }
-
-//        Spacer(modifier = Modifier.height(20.dp))
-
-        }
-    }
-
-
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        MyApplicationTheme {
-            ProfileImage()
         }
     }
 }
+
+@Composable
+fun ColumnRowTest1() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .background(Color.Gray)
+    ) {
+        Text(
+            text = "안녕ㅎ",
+            color = Color.White,
+            fontSize = 20.sp
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+//            horizontalArrangement = Arrangement.SpaceAround
+//            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(text = "왼쪽")
+            Text(text = "중앙")
+            Text(text = "오른쪽")
+        }
+
+        Text(
+            text = "하이",
+            color = Color.Blue,
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Composable
+fun ProfileImage() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+//            .padding(5.dp)
+            .background(color = Color(0xFF000000))
+            .padding(10.dp)
+            .border(
+                border = BorderStroke(5.dp, color = Color(0xFF323232))
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Box(
+            modifier = Modifier.padding(top = 50.dp)
+
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.mokoko1),
+                contentDescription = "콰앙",
+                modifier = Modifier
+                    .size(200.dp)
+            )
+        }
+
+        Text(
+            text = "Android Developer",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(20.dp)
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        ) {
+            Text(
+                text = "Email",
+                color = Color.White,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+
+            Text(
+                text = "snoopn@naver.com",
+                color = Color.White,
+                fontSize = 14.sp,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+
+        ) {
+            Text(
+                text = "Github",
+                color = Color.White,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+
+            Text(
+                text = "https://github.com/KyungHwa0",
+                color = Color.White,
+                fontSize = 14.sp,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        ) {
+            Text(
+                text = "Blog",
+                color = Color.White,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+
+            Text(
+                text = "https://wagzack.tistory.com/",
+                color = Color.White,
+                fontSize = 14.sp,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+
+//        Spacer(modifier = Modifier.height(20.dp))
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        ProfileImage()
+    }
+}
+
