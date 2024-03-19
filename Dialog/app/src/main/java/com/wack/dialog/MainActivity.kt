a9e8a9e6033434c652bc31c8ed8a2a4749f403ac
kyunghwa <snoopn303@gmail.com>
2024-03-20 02:13:08 +0900

    dialog 응용

--- Dialog/app/src/main/java/com/wack/dialog/MainActivity.kt
+++ Dialog/app/src/main/java/com/wack/dialog/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
@@ -12,8 +14,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.wack.dialog.ui.theme.DialogTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DialogEx()
+                    CustomDialog()
                 }
             }
         }
@@ -78,10 +83,57 @@ fun DialogEx() {
     }
 }
 
+@Composable
+fun CustomDialog() {
+    var openDialog by remember { mutableStateOf(false) }
+    var counter by remember { mutableStateOf(0) }
+
+    Column {
+        Button(onClick = {
+            openDialog = true
+        }) {
+            Text("다이얼로그 열기")
+        }
+        Text("카운터: $counter")
+    }
+
+    if (openDialog) {
+        Dialog(onDismissRequest = {
+            openDialog = false
+        }) {
+            Surface {
+                // Column 안에 Low를 만들어 Button을 수평 정렬
+                Column(modifier = Modifier.padding(8.dp)) {
+                    Text("카운터를 조작해봅시다.")
+                    Row(modifier = Modifier.align(Alignment.End)) {
+                        Button(onClick = {
+                            counter--
+                            openDialog = false
+                        }) {
+                            Text("-1")
+                        }
+                        Button(onClick = {
+                            counter++
+                            openDialog = false
+                        }) {
+                            Text("+1")
+                        }
+                        Button(onClick = {
+                            openDialog = false
+                        }) {
+                            Text("취소")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     DialogTheme {
-        DialogEx()
+        CustomDialog()
     }
 }
